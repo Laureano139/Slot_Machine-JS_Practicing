f2258b2e3f7e44624b10b50b4e016911f9743758
Hendrix <afamorim16@sapo.pt>
2024-08-18 01:20:17 +0100

    Added context documentation

--- project.js
+++ project.js
@@ -3,6 +3,12 @@ const prompt = require('prompt-sync')();
 
 // DEPOSIT MONEY
 
+/**
+ * A deposit should be a positive number, either an integer or a float.
+ * If the user enters a negative number, a string, or a number equal to 0, the program should 
+ * ask the user to enter a valid amount, on loop, until the user enters a valid amount.
+ */
+
 const deposit = () => {
     while(true) {
         const amountStr = prompt('Enter amount to deposit: ');
@@ -22,6 +28,12 @@ const deposit = () => {
 
 // NUMBER OF LINES TO BET ON
 
+/**
+* In a typical slot machine, the player can bet on multiple lines.
+* In this case, the number of lines is a positive number between 1 and 3, each representing
+* the first line (1), the center line (2) and the last line (3).
+*/
+
 const numberOfLinesBet = () => {
     while(true) {
         const linesStr = prompt('How many lines would you like to bet on (1-3)? >> ');
@@ -37,6 +49,12 @@ const numberOfLinesBet = () => {
 }
 
 // BET AMOUNT
+
+/**
+ * The bet amount should be a positive number, either an integer or a float.
+ * The bet amount should be less than or equal to the balance divided by the number of lines (*).
+ * (*) -> If the balance is 10€ and the player bets on 3 lines, the maximum bet amount is 10€ / 3 = 3.33€ per line.
+ */
 
 const getBetAmount = (balance, numLines) => {
     while(true) {
